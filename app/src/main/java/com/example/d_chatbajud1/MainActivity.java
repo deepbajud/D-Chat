3b4c74f27a9c5032e0f1757afeb7d24168d0976d
deepbajud <dbajud@gmail.com>
2023-08-10 01:04:12 +0530

    commit 2

--- app/src/main/java/com/example/d_chatbajud1/MainActivity.java
+++ app/src/main/java/com/example/d_chatbajud1/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.d_chatbajud1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
 import com.hbb20.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +38,86 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mcountrycodepicker = findViewById(R.id.CountryCodePicker);
+        msendotp = findViewById(R.id.btnSend_otp);
+        mgetphonenumber = findViewById(R.id.edtPhone_Number);
+        mprogressbarofmain = findViewById(R.id.progressbar);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        countrycode = mcountrycodepicker.getSelectedCountryCodeWithPlus();
+
+        mcountrycodepicker.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected() {
+                countrycode = mcountrycodepicker.getSelectedCountryCodeWithPlus();
+            }
+        });
+
+        msendotp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String number;
+                number = mgetphonenumber.getText().toString();
+                if (number.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please Enter Your Mobile Number", Toast.LENGTH_SHORT).show();
+                } else if (number.length() < 10) {
+                    Toast.makeText(getApplicationContext(), "Please Enter correct number", Toast.LENGTH_SHORT).show();
+                } else {
+                    mprogressbarofmain.setVisibility(view.VISIBLE);
+                    phonenumber = countrycode + number;
+
+                    PhoneAuthOptions options = PhoneAuthOptions.newBuilder(firebaseAuth)
+                            .setPhoneNumber(phonenumber)
+                            .setTimeout(69L, TimeUnit.SECONDS)
+                            .setActivity(MainActivity.this)
+                            .setCallbacks(mCallbacks)
+                            .build();
+
+                    PhoneAuthProvider.verifyPhoneNumber(options);
+
+
+                }
+
+
+            }
+        });
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                //space for automatically fetch otp at this thime deep don't know about this code
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                Toast.makeText(getApplicationContext(), "OTP is Sent", Toast.LENGTH_SHORT).show();
+                mprogressbarofmain.setVisibility(View.INVISIBLE);
+                codesent = s;
+                Intent intent = new Intent(MainActivity.this, Signing_Activity.class);
+                intent.putExtra("otp", codesent);
+                startActivity(intent);
+            }
+        };
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(MainActivity.this, Chat_Activity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
 
 
     }
--- app/src/main/java/com/example/d_chatbajud1/Signing_Activity.java
+++ app/src/main/java/com/example/d_chatbajud1/Signing_Activity.java
@@ -3,12 +3,34 @@ package com.example.d_chatbajud1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Signing_Activity extends AppCompatActivity {
+
+    TextView mchangenumber;
+    EditText mgetotp;
+    android.widget.Button mverifyotp;
+    String enteredotp;
+    FirebaseAuth firebaseAuth;
+    ProgressBar mprogressbarofotpauth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signing);
+
+        mchangenumber=findViewById(R.id.changenumber);
+        mverifyotp=findViewById(R.id.verifyotp);
+        mgetotp=findViewById(R.id.getotp);
+        mprogressbarofotpauth=findViewById(R.id.progressbarofotpauth);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+
+
+
     }
 }
